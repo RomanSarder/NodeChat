4fabcfcdca4706ffaa49a995f280a5e88ab22acd
Роман <roman.sarder@yandex.ru>
2017-04-15 18:03:53 +0300

    Greet user and alert others

--- server/server.js
+++ server/server.js
@@ -13,6 +13,17 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
     console.log('New user connected');
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat',
+        createdAt: new Date().getTime()
+    });
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New User joined the room',
+        createdAt: new Date().getTime()
+
+    });
     socket.on('disconnect', () => {
         console.log('Client disconencted');
     });
@@ -23,6 +34,11 @@ io.on('connection', (socket) => {
             text: newMessage.text,
             createdAt: new Date().getTime()
         });
+        // socket.broadcast.emit('newMessage', {
+        //     from: newMessage.from,
+        //     text: newMessage.text,
+        //     createdAt: new Date().getTime()
+        // })
     })
 });
 
