4d8f5870c0a1b0b93cdfe12baae5d7e9edb722c8
Роман <roman.sarder@yandex.ru>
2017-04-17 12:30:05 +0300

    Add geolocation api support

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '../public');
 const server = http.createServer(app);
 const io = socketIO(server);
-const { generateMessage } = require('./utils/message');
+const { generateMessage, generateLocationMessage } = require('./utils/message');
 
 
 app.use(express.static(publicPath));
@@ -24,12 +24,10 @@ io.on('connection', (socket) => {
         console.log('create email', newMessage);
         io.emit('newMessage', generateMessage(newMessage.from, newMessage.text));
         callback('This is from the server');
-        // socket.broadcast.emit('newMessage', {
-        //     from: newMessage.from,
-        //     text: newMessage.text,
-        //     createdAt: new Date().getTime()
-        // })
-    })
+    });
+    socket.on('createLocationMessage', (coords) => {
+        io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+    });
 });
 
 server.listen(port, function() {
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,11 +1,19 @@
 const expect = require('expect');
-const { generateMessage } = require('./message');
+const { generateMessage, generateLocationMessage } = require('./message');
 
 describe('generateMessage', () => {
     it('should generate valid message object ', () => {
         let res = generateMessage('Admin', 'Text');
         expect(res.from).toEqual('Admin');
         expect(res.text).toEqual('Text');
+        expect(res.createdAt).toBeA('number');
+    });
+});
+describe('generateLocationMessage', () => {
+    it('should generate correct location object', () => {
+        let res = generateLocationMessage('Admin', 1, 1);
+        expect(res.url).toEqual('https://www.google.com/maps?q=1,1');
+        expect(res.from).toEqual('Admin');
         expect(res.createdAt).toBeA('number');
     });
 });
