daaa5d7f69726151a5fef6252df4a124b93b5f44
Роман <roman.sarder@yandex.ru>
2017-04-17 15:28:57 +0300

    add templating

--- public/js/index.js
+++ public/js/index.js
@@ -8,19 +8,30 @@ socket.on('disconnect', function() {
     console.log('Disconnected');
 });
 socket.on('newMessage', function(message) {
+    let template = jQuery('#message-template').html();
     let formattedTime = moment(message.createdAt).format('h:mm a');
-    let li = jQuery('<li></li>');
-    li.text(`${message.from} ${formattedTime}: ${message.text}`);
-    jQuery('#messages').append(li);
+    let html = Mustache.render(template, {
+        text: message.text,
+        from: message.from,
+        createdAt: formattedTime
+    });
+
+    jQuery('#messages').append(html);
+
+    // let li = jQuery('<li></li>');
+    // li.text(`${message.from} ${formattedTime}: ${message.text}`);
+    // jQuery('#messages').append(li);
 });
 socket.on('newLocationMessage', function(message) {
+    let template = jQuery('#location-message-template').html();
     let formattedTime = moment(message.createdAt).format('h:mm a');
-    let li = jQuery('<li></li>');
-    let a = jQuery('<a target="_blank">My current location</a>');
-    li.text(`${message.from} ${formattedTime}: `);
-    a.attr('href', message.url);
-    li.append(a);
-    jQuery('#messages').append(li);
+    let html = Mustache.render(template, {
+        url: message.url,
+        from: message.from,
+        createdAt: formattedTime
+    });
+
+    jQuery('#messages').append(html);
 
 });
 
